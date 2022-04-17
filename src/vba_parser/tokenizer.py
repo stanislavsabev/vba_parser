e5c2fa0b88dc5abaa13041594cc8376091ab109a
stanislavsabev <bezraboten.34@gmail.com>
2022-04-17 17:32:42 +0200

    Tokenizer to parse Number.

--- src/vba_parser/tokenizer.py
+++ src/vba_parser/tokenizer.py
@@ -1,11 +1,15 @@
 """A tokenizer to pull and categorize the lexical syntax of a VBA code."""
 import re
-
+from typing import Optional
+from collections import namedtuple
 from vba_parser import spec
 
 
 class UnknownTokenError(Exception):
     pass
+
+
+Token = namedtuple('Token', ['type', 'value'], defaults=['', None])
 
 
 class Tokenizer:
@@ -15,17 +19,43 @@ class Tokenizer:
     """
 
     def __init__(self) -> None:
-        """Init tokenizer."""
-        self._code = ''
-        self._cursor_position = 0
+        """Constructor (not used)."""
+        self._string = ''
+        self._cursor = 0
 
-    def Number(self, statement: str) -> int:
-        """Parses Number token.
+    def init(self, string: str) -> None:
+        """Init Tokenizer.
 
         Args:
-            statement: A string to match.
+            string: A string, program input.
         """
-        m = re.match(spec.Number, statement)
-        if m is None:
-            raise UnknownTokenError
-        return int(m.string)
+        self._string = string
+        self._cursor = 0
+
+    def has_more_tokens(self) -> bool:
+        """Return `True`, unless the end if input is reached."""
+        return self._cursor < len(self._string)
+
+    def next_token(self) -> Optional[Token]:
+        """Match next token accorting to the spec.
+
+        Returns:
+            A Token, or None, if the end of input is reached.
+
+        Raises:
+            UnknownTokenError if no token is matched.
+        """
+        if not self.has_more_tokens():
+            return None
+        string = self._string[self._cursor:]
+
+        # Match Number
+        match = re.match(spec.Number, string)
+        if match:
+            return self.number(match.string)
+
+        raise UnknownTokenError(f'Unknown token at {self._cursor}')
+
+    def number(self, string) -> int:
+        """Parse a `NUMBER` token."""
+        return Token(type='NUMBER', value=int(string))
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,11 +1,31 @@
 """Test start module."""
 
-from vba_parser import tokenizer as t
+import pytest
+
+from vba_parser import tokenizer
+
+
+@pytest.fixture
+def tokenizer_fixture() -> tokenizer.Tokenizer:
+    return tokenizer.Tokenizer()
 
 
 def test_create():
     """Test start.here."""
-    tokenizer = t.Tokenizer()
-    assert tokenizer is not None
-    assert tokenizer._code == ''
-    assert tokenizer._cursor_position == 0
+    sut = tokenizer.Tokenizer()
+    assert sut is not None
+    assert sut._string == ''
+    assert sut._cursor == 0
+
+
+def test_init(tokenizer_fixture):
+    """Test start.here."""
+    sut = tokenizer.Tokenizer()
+    sut.init('abc')
+    assert sut._string == 'abc'
+    assert sut.has_more_tokens()
+
+
+def test_number(tokenizer_fixture, random_positive_int):
+    """Test the number Token production."""
+    tokenizer_fixture.number(str(random_positive_int))
