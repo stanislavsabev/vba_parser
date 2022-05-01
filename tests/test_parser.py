f2a7642f4334a9992da3569c6afbb9a405c7420b
Stanislav Sabev <ssabev@google.com>
2022-05-02 00:50:16 +0200

    Create tests for `Parser._consume` + refactor.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,14 +1,15 @@
 """Test start module."""
 
+from unittest.mock import MagicMock
+import mock
 import pytest
 
 from vba_parser import parser
-from vba_parser import tokenizer
-from vba_parser.tokenizer import Token
+from vba_parser import tokenizer as tk
 
 
 @pytest.fixture
-def parser_fixture():
+def parser_fixture() -> parser.Parser:
     return parser.Parser()
 
 
@@ -25,10 +26,9 @@ def test_create():
         ('"5"', dict(type='string_literal', value='"5"')),
     ]
 )
-def test_parse_program(code, token):
+def test_parse_program(parser_fixture, code, token):
     """Test program."""
-    p = parser.Parser()
-    actual = p.parse(code)
+    actual = parser_fixture.parse(code)
     expected = dict(
         type="Program",
         value=token
@@ -36,17 +36,18 @@ def test_parse_program(code, token):
     assert actual == expected
 
 
-def test_numeric_literal(random_positive_int):
+def test_numeric_literal(parser_fixture, random_positive_int):
     """Test numeric literal."""
+    p = parser_fixture
     number = random_positive_int
-    p = parser.Parser()
-    p._lookahead = tokenizer.Token(type='NUMBER', value=number)
+    p._lookahead = tk.Token(type='NUMBER', value=number)
     actual = p.numeric_literal()
     expected = dict(
         type="numeric_literal",
         value=number,
     )
     assert actual == expected
+
 
 @pytest.mark.parametrize('string', [
     '"hello ""world""!"',
@@ -61,12 +62,40 @@ def test_numeric_literal(random_positive_int):
     pytest.param('""""asdf""', marks=pytest.mark.xfail),
     pytest.param('"""asdf"""', marks=pytest.mark.xfail),
 ])
-def test_string_literal(string):
-    p = parser.Parser()
-    p._lookahead = tokenizer.Token(type='STRING', value=string)
+def test_string_literal(parser_fixture, string):
+    p = parser_fixture
+    p._lookahead = tk.Token(type='STRING', value=string)
     actual = p.string_literal()
     expected = dict(
         type="string_literal",
         value=string,
     )
     assert actual == expected
+
+
+@pytest.mark.parametrize('token_type, token_value', [
+    ('NUMBER', 5),
+    ('STRING', '"ABC"'),
+    ('STRING', '"5"'),
+])
+def test_consume_calls_next_token(parser_fixture, token_type, token_value):
+    """Parser consume to call next token after it consumes valid token."""
+    p = parser_fixture
+    lookahead_mock = MagicMock()
+    lookahead_mock.type = token_type
+    lookahead_mock.value = token_value
+    p._lookahead = lookahead_mock
+    tokenizer_mock = MagicMock()
+    p._tokenizer = tokenizer_mock
+
+    p._consume(token_type)
+    tokenizer_mock.next_token.assert_called_once()
+
+
+@mock.patch.object(tk.Tokenizer, 'next_token')
+def test_consume_raises_on_eof_token(next_token_mock, parser_fixture):
+    p = parser_fixture
+    p._lookahead = tk.EOF_TOKEN
+    with pytest.raises(SyntaxError, match=r'Unexpected end of input'):
+        p._consume('STRING')
+    next_token_mock.assert_not_called()
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -18,16 +18,16 @@ def test_create():
     assert sut._cursor == 0
 
 
-def test_init(tokenizer_fixture):
+def test_init():
     """Test start.here."""
     sut = tokenizer.Tokenizer()
     sut.init('abc')
     assert sut._string == 'abc'
     assert sut.has_more_tokens()
 
+
 def test_number_token(tokenizer_fixture, random_positive_int):
     """Test the number Token production."""
-    
     expected = tokenizer.Token(type='NUMBER', value=random_positive_int)
     actual = tokenizer_fixture.number_token(str(random_positive_int))
     assert actual == expected
@@ -41,11 +41,8 @@ def test_number_token(tokenizer_fixture, random_positive_int):
     '"""a""""sd"""""',
     '"asdf"',
 ])
-def test_number_token(tokenizer_fixture, vba_string):
+def test_string_token(tokenizer_fixture, vba_string):
     """Test the string Token production."""
-    
     expected = tokenizer.Token(type='STRING', value=vba_string)
     actual = tokenizer_fixture.string_token(str(vba_string))
     assert actual == expected
-
-
