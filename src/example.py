4c6a65efd5da2144290e4890733e5eda6c756b96
Stanislav Sabev <ssabev@google.com>
2022-05-01 23:15:30 +0200

    Update example to print formatted AST.

--- src/example.py
+++ src/example.py
@@ -1,11 +1,13 @@
 """Example for how to use the `vba_parser` package."""
 
+import json
 from vba_parser import parser
 
 
 def main():
     """Entry point for this script."""
-    print(parser.Parser().parse('5'))
+    ast = json.dumps(parser.Parser().parse('5'), indent=4)
+    print(ast)
 
 
 if __name__ == '__main__':
